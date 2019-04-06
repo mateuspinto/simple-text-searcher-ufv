8b449fd1456f673f0227e3f83385676824e6742f
Mateus P. Silva <mateus.p.silva@ufv.br>
2019-04-06 20:53:09 -0300

    a

--- main.c
+++ main.c
@@ -2,10 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include <dirent.h>
-#include <unistd.h>
-#include <limits.h>
-
 #include "adt/tstNode.h"
 #include "adt/invertedChainedList.h"
 #include "adt/tstFileNode.h"
