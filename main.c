c5b5ce9af2a2ab4df4d7f1b174415d693b0a50b6
Mateus P. Silva <mateus.p.silva@ufv.br>
2019-04-13 12:28:55 -0300

    arrumado inputs

--- main.c
+++ main.c
@@ -9,11 +9,9 @@
 
 int main() {
 
-
     tstFileNode * tstFileInputs, *tstFileStopWords;
     tstNode * tstAutoFill, * tstStopWords;
     patriciaNode * patricia;
-    int a;
 
     tstNodeStartTree(&tstAutoFill);
     tstNodeStartTree(&tstStopWords);
@@ -22,18 +20,11 @@ int main() {
     tstFileNodeStartTree(&tstFileStopWords);
 
     tstFileNodeInsertInputs(&tstFileStopWords, "stopwords");
-    tstFileNodeInsertInputs(&tstFileInputs, "inputs_test");
+    tstFileNodeInsertInputs(&tstFileInputs, "inputs");
 
     generalFunctionsLoadTstFileOnlyTST(&tstFileStopWords, &tstStopWords);
     generalFunctionsLoadTstFile(&tstFileInputs, &tstAutoFill, &tstStopWords, &patricia);
 
-    /* patriciaNodeInsertWord(&patricia, "love", "bohemian");
-    patriciaNodeInsertWord(&patricia, "love", "bohemian");
-    patriciaNodeInsertWord(&patricia, "love", "bohemian");
-    patriciaNodeInsertWord(&patricia, "love", "bohemian");
-    patriciaNodeInsertWord(&patricia, "love", "bohemian");
-    patriciaNodeInsertWord(&patricia, "lo", "bohemian");
-    patriciaNodeInsertWord(&patricia, "hello", "bohemian"); */
     patriciaNodeGoThroughWithOcurrences(&patricia);
 
 
